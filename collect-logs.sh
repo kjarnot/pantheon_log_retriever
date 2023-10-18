994ae7ae072c48e3d52e21ce17fd40b9fc502fab
Kevin Jarnot <kevin.jarnot@uptimeinstitute.com>
2023-10-18 18:50:24 +0000

    Added directory arg

--- collect-logs.sh
+++ collect-logs.sh
@@ -7,6 +7,7 @@ helpFunction()
    echo "Usage: $0 -u=YOURSITEUUID -e=YOURSITEENV"
    echo -e "\t-u Site UUID from Dashboard URL"
    echo -e "\t-e Environment name such as dev, test or live"
+   echo -e "\t-d directory for log files"
    exit 1 # Exit script after printing help
 }
 
@@ -22,6 +23,10 @@ case $i in
     ENV_NAME="${i#*=}"
     shift # past argument=value
     ;;
+    -d=*)
+    DIR_NAME="${i#*=}"
+    shift # past argument=value
+    ;;
     --default)
     DEFAULT=YES
     shift # past argument with no value
@@ -33,7 +38,7 @@ esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$SITE_UUID" ] && [ -z "$ENV_NAME" ] 
+if [ -z "$SITE_UUID" ] && [ -z "$ENV_NAME" ] && [ -z "$DIR_NAME" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
@@ -43,7 +48,7 @@ fi
 # and put them in an app_server directory where you execute this script
 # and put them in an app_server directory where you execute this script
 echo "Retrieving appserver logs..."
-rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* app_server_logs
+rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* $DIR_NAME/app_server_logs
 
 # Check to see if the live environment is called, then grab the logs from all app containers
 echo "$ENV_NAME"
@@ -51,18 +56,18 @@ if [ $ENV_NAME == 'live' ]; then
   for app_server in `dig +short appserver.live.$SITE_UUID.drush.in`
     do
       echo "Retrieving appserver logs from $app_server..."
-      rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* app_server_logs_$app_server
+      rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* $DIR_NAME/app_server_logs_$app_server
     done
   for app_server in `dig +short dbserver.live.$SITE_UUID.drush.in`
     do
       echo "Retrieving dbserver logs from $app_server..."
-      rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@dbserver.$ENV_NAME.$SITE_UUID.drush.in:logs db_server_logs_$app_server
+      rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@dbserver.$ENV_NAME.$SITE_UUID.drush.in:logs $DIR_NAME/db_server_logs_$app_server
     done
 else
    echo "Retrieving appserver logs..."
-   rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* app_server_logs_$ENV_NAME
+   rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* $DIR_NAME/app_server_logs_$ENV_NAME
    # Grab the mysqld-slow-query.log, mysqld.log and put them in a
    # db_server directory
    echo "Retrieving database server logs..."
-   rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"//' $ENV_NAME.$SITE_UUID@dbserver.$ENV_NAME.$SITE_UUID.drush.in:logs db_server_logs_$ENV_NAME
+   rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222 -o "StrictHostKeyChecking no"//' $ENV_NAME.$SITE_UUID@dbserver.$ENV_NAME.$SITE_UUID.drush.in:logs $DIR_NAME/db_server_logs_$ENV_NAME
 fi
